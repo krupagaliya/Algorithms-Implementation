6893557927509c88f708768caf9ca532d4a3c435
Krupa Galiya <krupagaliya@vgecg.ac.in>
2019-01-15 21:54:48 +0530

    final compressed map got

--- C/Shannon.cpp
+++ C/Shannon.cpp
@@ -2,16 +2,16 @@
 #include <fstream>
 #include <type_traits>
 using namespace std;
-
+int str_pos=0;
 // void probability(string);
 // int count1(string,char);
 string stacks(string final);
 string recusion(vector<int> & v);
-vector<int> sorting(const std::map<char,int>&);
+vector<int> sorting(const std::map<int,char>&);
 
 // void shannon(vector<int>&left, vector<int>& right,string ls,string rs,vector<int> v);
-set<string> shannon(string rs,int n,map<char,int>& m);
-void mapping(set<string>& s1,map<char,int>& probability_map,vector<int>& v);
+map<string,char> shannon(string rs,int n,map<int,char>& m);
+void mapping(set<string>& s1,map<int,char>& probability_map,vector<int>& v);
 
 string removeSpaces(string str) 
 { 
@@ -29,7 +29,7 @@ string get_input()
 {
   fstream f("input.txt", fstream::in );
   string s;
-  getline( f, s, '\0');
+  getline( f, s);
   f.close();
   s = removeSpaces(s);
   cout << s << endl; 
@@ -49,48 +49,43 @@ int count1(string s, char c)
 	return count_var;
 }
 
-map<char,int> probability(string str)
+map<int,char> probability(string str)
 {
    str = removeSpaces(str);
-   map<char,int> mymap;
-   map<char,int>::iterator it;
+   map<int,char> mymap;
+   map<int,char>::iterator it;
    int counter;
    char c;  
    for(auto it = str.begin() ; it != str.end(); ++it)
 	{
-	 c = (char)*it;
-	 // cout<<"ok"<<str<<"pk"<<endl;
-	
+	 c = (char)*it;	
 	 counter = count1(str,c);
-	 mymap.insert(pair<char, int>(c, counter));
+	  mymap.insert(pair<int, char>(counter, c));
 	}
-	it = mymap.begin();
-	// mymap.erase(it);
-	// it++;
-	// mymap.erase(it);
-	// it++;
-   // cout<<"okay";
+	// it = mymap.begin();
+	
    return mymap;
 
 }
-vector<int> sorting(const std::map<char,int>& mymap)
+vector<int> sorting(const std::map<int,char>& mymap)
 {
-	map<char,int> element;
-typedef function<bool(std::pair<char, int>, std::pair<char, int>)> Comparator;
+	map<int,char> element;
+typedef function<bool(std::pair<int, char>, std::pair<int, char>)> Comparator;
 Comparator compFunctor =
-		[](pair<char, int> elem1 ,pair<char, int> elem2)
+		[](pair<int, char> elem1 ,pair<int, char> elem2)
 		{
-			return elem1.second >= elem2.second;
+			return elem1.first >= elem2.first;
 		};
-      set<pair<char, int>, Comparator> setOfWords(mymap.begin(), mymap.end(), compFunctor);
-	vector<int> vec;
+      set<pair<int, char>, Comparator> setOfWords(mymap.begin(), mymap.end(), compFunctor);
+vector<int> vec;
 	vec.reserve(mymap.size());
-
- for (pair<char, int> mymap : setOfWords)
+ cout<<"size is"<<vec.size();
+ for (pair<int, char> mymap : setOfWords)
  {
 	cout << mymap.first << " :: " << mymap.second <<endl;
-	vec.push_back(mymap.second);
+	vec.push_back(mymap.first);
  }
+
   return vec;
 }
 
@@ -171,19 +166,45 @@ string recusion(vector<int> & v) {
 	//   	cout<<"\n"<< *it<<"\t";
 	// }
 // }
-set<string> shannon(string rs,int n,map<char,int>& m)
+
+string split(string final, string word)
+{
+  int i=0;
+  int inttemp;
+  int temp;
+ char spc = ' ';
+  string s="";
+  for(i=str_pos;i<final.size();i++)
+  {
+    
+   if (final[i]==spc)
+   {
+     // cout<<"od"<<i<<endl;
+      temp = i;
+      break;
+   }
+  }
+      
+ 
+ 
+ 
+  inttemp= temp - str_pos;
+  s = final.substr(str_pos,inttemp);
+  if (temp!=0)
+    str_pos=temp+1;  
+  return s;
+}
+map<string,char> shannon(string rs,int n,map<int,char>& m)
 { 
 	string final = "";
   // map<char, int>::iterator itm ; 
-  vector<char> myvec;
-  vector<char>::iterator it;
-  set<string> set1;
 	string stk;
 	int i=0;
 	final.append("0");
   final.append(" ");
+
 	string ls = "0";
-    for(i=1;i<n-2;i++)
+    for(i=1;i<n-1;i++)
     {
     	// cout<<"\n if "<<rs[i];
        final.append(rs,0,i);
@@ -194,151 +215,45 @@ set<string> shannon(string rs,int n,map<char,int>& m)
     cout<<"\n rs is \t"<<rs;
     final.append(rs,0,i);
     final.append(" ");
-    cout<<"\n ======= final is "<<final<<endl;
-
+    cout<<"\n ======= final is"<<final<<endl;
+    
    
     string word;
-    multimap<char,string> map1;
-    istringstream iss(final, istringstream::in);
+    map<string,char> map1;
+    
   
   char ch;
-   for(std::map<char, int>::iterator itm =m.begin(); itm != m.end(); itm++)
-   {
+  string temp_str;
+  std::map<int,char>::reverse_iterator rit;
   
-    // while( iss >> word )     
-    // {
-     ch = (char)itm->first;
-    // int i=0;
-    //    cout<<"\ti is "<<i<<"\t"<<ch;
-    //      i++;
-     
-        map1.insert(pair<char,string>(ch,"abd"));
-      
-    // }
-    }
-    
-      // map1.insert ( std::pair<char,string>('a',"100") );
-      // map1.insert('A',s.c_str());
-      // map1.insert('B',s.c_str());
-      // map1.insert('C',s.c_str());
-    // printf( "%s \n", s.c_str() );
+  for (rit=m.rbegin(); rit!=m.rend(); ++rit)
+  {
+        ch = (char)rit->second;
+        temp_str = split(final,word);
+       map1.insert(pair<string,char>(temp_str,ch));
+     // ch = (char)rit->second;
+     //  map1.insert(pair<char,string>(ch,"abd"));
+ 
+  } 
+   std::map<string,char>::iterator itm;
   
-    for (auto it=map1.begin(); it != map1.end(); ++it) 
-    cout << ' ' << it->first<< ' ' <<it->second<<endl;
-    
-
-
-    string stks;
-    int j=0;
-     char c = '0';
-     for (int i = 0; i < final.size(); i++)
-     {
-     	  myvec.push_back(final[i]);
-     	 
-     	 if (final[i]==c)
-     	 {
-     	 	j++;
-     	 	for(it=myvec.begin();it!=myvec.end();it++)
-     	 	{
-             string stk(myvec.begin(), myvec.end());
-             set1.insert(stk);             
-             stks = stk;  
-     	  	 }
-     	  	 myvec.clear();
-     	 	}
-        }    
-  cout<<"\n ================= \n";
-     for (auto it=set1.begin(); it != set1.end(); ++it) 
-		cout << ' ' << *it; 
-	cout<<"\n ================= \n";
-  return set1;
+  for (itm=map1.begin(); itm!=map1.end(); itm++)
+  {
+   
+  cout << "(" << (*itm).first << ", "
+      << (*itm).second << ")" << endl;
+ } 
+  return map1;
 }
 
-void mapping(set<string>& set1,map<char,int>& m,vector<int>& v)
-{
-	multimap<int,string> m2;
-	map<char,int>::iterator itm;
-	set<string>::iterator its;
-	// vector<int>::iterator itv;
-	for (auto it=set1.begin(); it != set1.end(); ++it) 
-		cout << ' ' << *it;
-	
-	// for (auto it=m.begin(); it != m.end(); ++it) 
-	// 	// cout << ' ' << it->first<< ' ' <<it->second<<endl;
-	// {
-	// 	if it-second>
-	// }
-
-     
-  //  map<string,>
-  //  for (auto it=v.begin(); it != v.end(); ++it) 
-		// cout << ' ' << *it;
-	 // cout<<"\n DOne";
-  
-  //   itm = m.begin();
-    
-  //   left.push_back(*it);
-	 // cout<<itm->first;
-  //       for (int i=0;i <5;i++) 
-  //       {
-        	
-		//          // m2.insert(pair<int, string>(,"haha"));
-		// }
-	
- // //  cout<<endl;
-	// 	cout<<"okay";
-	// for (auto it=m2.begin(); it != m2.end(); ++it) 
-	// {
-	// 	for (auto it=v.begin(); it != v.end(); ++it) 
-	// 	m2.insert(pair<int, string>(*it,"haha"));
-	// }
-    
-    // vector<int>::iterator itv;
-   // map<char,int>::iterator itm;
-   // set<string>::iterator its;
-   
-   
-   // int i = 0;
-   // itm = m.begin();
-   // its = set1.begin();
-   // cout<<
-   // // m2.insert(pair<int,string>(*itm,*its));
-   // while(i< m2.size())
-   // { 
-   // m2.insert(pair<int,string>(itm->second,*its));
-   // itm++;
-   // its++;
-    // }
-
-   //  for (auto it=v.begin(),set1.begin(); it != v.end(),set1.end(); ++it) 
-   // {
-		 //      m2.insert(pair<int, string>(*it,"fg"));
-   // }
-   // for (auto its=set1.begin(); its != set1.end(); ++its) 
-   // {
-   // 	 m2.insert(pair<int, string>(12,*its));
-   // }
-
-   // cout<<"okay2";
-
-   for (auto it=m2.begin(); it != m2.end();++it) 
-		cout <<"\n" <<' ' << it->first<< ' ' <<it->second<<endl;
-	// vector<int> v( m.size() );
- //     transform( m.begin(), m.end(), v.begin(), get_second );
- //    sort( v.begin(), v.end() );
-
- //     for (int i=0; i<v.size(); i++) cout << v[i] << endl;
-     
-     	
-
-}
 
 
 int main()
 {
  string data;
- map<char,int>probability_map;
- map<char,int>::iterator it;
+ map<int,char>probability_map;
+ map<string,char> com;
+ map<int,char>::iterator it;
  vector<int> vec;
  string ls,rs;
  set<string> set1;
@@ -359,11 +274,22 @@ int main()
  rs = recusion(vec);
 
 
- set1 = shannon(rs,n,probability_map);
- mapping(set1,probability_map,vec);
+ com = shannon(rs,n,probability_map);
  
  //  std::copy(vec.begin(), vec.end(), std::ostream_iterator<int>(std::cout, " , ") );
 	// std::cout<<endl;
 
+  
+
+ 
+  for (it=probability_map.begin() ; it!=probability_map.end() ; it++) 
+    cout << "(" << (*it).first << ", "
+      << (*it).second << ")" << endl;
+  
+
+     //reverse iterator
+  //     std::map<int,char>::reverse_iterator rit;
+  // for (rit=probability_map.rbegin(); rit!=probability_map.rend(); ++rit)
+  //   std::cout << rit->first << " => " << rit->second << '\n';
 
 }
